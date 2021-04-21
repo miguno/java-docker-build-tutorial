87a598eaa0f6f57d6cf1bec3015ed4ced63851c3
Michael G. Noll <miguno@miguno.com>
2021-04-21 10:22:44 +0200

    Upgrade to Junit 5

--- src/test/java/com/miguno/StatusTest.java
+++ src/test/java/com/miguno/StatusTest.java
@@ -1,29 +1,29 @@
 package com.miguno;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatusTest {
 
   private HttpServer server;
   private WebTarget target;
 
-  @Before
+  @BeforeEach
   public void setUp() {
     server = App.startServer();
     Client c = ClientBuilder.newClient();
     target = c.target(App.BASE_URI);
   }
 
-  @After
+  @AfterEach
   public void tearDown() {
     server.shutdownNow();
   }
