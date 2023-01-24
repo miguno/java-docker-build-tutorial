1e7aa91e2c35e61764ea42366a986593b803bf23
Michael G. Noll <miguno@miguno.com>
2023-01-24 16:49:45 +0100

    Improve setup for just and helper scripts

--- create_image.sh
+++ create_image.sh
@@ -5,13 +5,16 @@
 # `-o pipefail`: Prevent errors in a pipeline (`|`) from being masked
 set -uo pipefail
 
-declare -r IMAGE_NAME="miguno/java-docker-build-tutorial"
-declare -r IMAGE_TAG="latest"
+# Import environment variables from .env
+set -o allexport && source .env && set +o allexport
 
-echo "Building image '$IMAGE_NAME:$IMAGE_TAG'"
+echo "Building image '$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG'..."
+# TIP: Add `--progress=plain` to see the full docker output when you are
+# troubleshooting the build setup of your image.
+#
 # Force x86_64 as the platform.  This workaround is needed on Apple Silicon
 # machines.  Details at https://stackoverflow.com/questions/70736928/.
 declare -r DOCKER_OPTIONS="--platform linux/x86_64/v8"
 # Use BuildKit, i.e. `buildx build` instead of just `build`
 # https://docs.docker.com/build/
-docker buildx build $DOCKER_OPTIONS -t "$IMAGE_NAME":"$IMAGE_TAG" .
+docker buildx build $DOCKER_OPTIONS -t "$DOCKER_IMAGE_NAME":"$DOCKER_IMAGE_TAG" .
---
+++ justfile
@@ -0,0 +1,30 @@
+# This justfile requires https://github.com/casey/just
+
+# Load environment variables from `.env` file.
+set dotenv-load
+
+# print available targets
+default:
+    @just --list --justfile {{justfile()}}
+
+# evaluate and print all just variables
+evaluate:
+    @just --evaluate
+
+# create a docker image (requires Docker)
+docker-image-create:
+    @echo "Creating a docker image ..."
+    @./create_image.sh
+
+# size of the docker image (requires Docker)
+docker-image-size:
+    @docker images $DOCKER_IMAGE_NAME
+
+# run the docker image (requires Docker)
+docker-image-run:
+    @echo "Running container from docker image ..."
+    @./start_container.sh
+
+# send request to the app's HTTP endpoint (requires running container)
+send-request-to-server:
+    curl http://localhost:8123/status
