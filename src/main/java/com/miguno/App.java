b55c105ce13eb368e5852be14cdd22e2b08967d5
Michael G. Noll <miguno@miguno.com>
2024-03-11 10:43:22 +0100

    Update to jersey 3.1.5

--- src/main/java/com/miguno/App.java
+++ src/main/java/com/miguno/App.java
@@ -8,26 +8,28 @@ import java.net.URI;
 
 public class App {
 
-  protected static final String BASE_URI = "http://0.0.0.0:8123/";
+    protected static final String BASE_URI = "http://0.0.0.0:8123/";
 
-  /**
-   * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-   */
-  protected static HttpServer startServer() {
-    // create a resource config that scans for JAX-RS resources and providers in com.miguno package
-    final ResourceConfig rc = new ResourceConfig().packages("com.miguno");
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     */
+    protected static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers in com.miguno package
+        final ResourceConfig rc = new ResourceConfig().packages("com.miguno");
 
-    // disable WADL
-    rc.property("jersey.config.server.wadl.disableWadl", true);
+        // disable WADL
+        rc.property("jersey.config.server.wadl.disableWadl", true);
 
-    // create and start a new instance of grizzly http server
-    // exposing the Jersey application at BASE_URI
-    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-  }
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
 
-  public static void main(String[] args) {
-    startServer();
-    System.out.println(String.format("Endpoint is available at %sstatus", BASE_URI));
-  }
+    public static void main(String[] args) {
+        HttpServer server = startServer();
+        if (server.isStarted()) {
+            System.out.println(String.format("Endpoint is available at %sstatus", BASE_URI));
+        }
+    }
 }
 
--- src/main/java/com/miguno/Status.java
+++ src/main/java/com/miguno/Status.java
@@ -1,16 +1,16 @@
 package com.miguno;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("status")
 public class Status {
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public String statusInformation() {
-    return "{\"status\": \"idle\"}\n";
-  }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String statusInformation() {
+        return "{\"status\": \"idle\"}\n";
+    }
 }
