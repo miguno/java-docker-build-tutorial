17e27c6622a210bd415dfc79a594075c681eeeae
Michael G. Noll <miguno@miguno.com>
2024-05-07 12:17:03 +0200

    Convert Status into a record class

--- src/main/java/com/miguno/javadockerbuild/Status.java
+++ src/main/java/com/miguno/javadockerbuild/Status.java
@@ -1,10 +1,3 @@
 package com.miguno.javadockerbuild;
 
-public class Status {
-
-  public String status;
-
-  public Status(String status) {
-    this.status = status;
-  }
-}
+public record Status(String status) {}
