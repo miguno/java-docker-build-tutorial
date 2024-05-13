f0cce68d434594b66e3053512c3265bd63c1c2b2
Michael G. Noll <miguno@miguno.com>
2024-05-13 14:43:04 +0200

    Force amd64 as the arch/platform on macOS Silicon devices

--- start_container.sh
+++ start_container.sh
@@ -8,15 +8,24 @@ set -uo pipefail
 # Import environment variables from .env
 set -o allexport && source .env && set +o allexport
 
+# shellcheck disable=SC2155
+declare -r OS="$(uname -s)"
+# "arm64" for Apple Silicon (M1/M2/M3/etc.)
+# shellcheck disable=SC2155
+declare -r ARCH="$(uname -m)"
+
 # Check requirements
 if ! command -v docker &> /dev/null; then
     echo "ERROR: 'docker' command not available. Is Docker installed?"
     exit 1
 fi
 
-# Force amd64 as the platform.  This workaround is needed on Apple Silicon
-# machines.  Details at https://stackoverflow.com/questions/72152446/.
-declare -r DOCKER_OPTIONS="--platform linux/amd64"
+DOCKER_OPTIONS=""
+if [[ "$OS" = "Darwin" && "$ARCH" = "arm64" ]]; then
+    # Force amd64 as the platform.  This workaround is needed on Apple Silicon
+    # machines.  Details at https://stackoverflow.com/questions/72152446/.
+    DOCKER_OPTIONS="--platform linux/amd64"
+fi
 
 echo "Starting container for image '$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG', exposing port ${APP_PORT}/tcp"
 echo "- Run 'curl http://localhost:${APP_PORT}/status' to send a test request to the containerized app."
