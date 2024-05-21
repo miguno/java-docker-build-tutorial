08b49fec71b5d44fac79b33747a2d7e8d4545507
Michael G. Noll <miguno@miguno.com>
2024-05-21 09:53:04 +0200

    Verify JSON

--- src/test/java/com/miguno/javadockerbuild/StatusControllerIT.java
+++ src/test/java/com/miguno/javadockerbuild/StatusControllerIT.java
@@ -1,12 +1,12 @@
 package com.miguno.javadockerbuild;
 
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StatusControllerIT {
@@ -16,6 +16,7 @@ public class StatusControllerIT {
   @Test
   public void getStatus() throws Exception {
     ResponseEntity<String> response = template.getForEntity("/status", String.class);
-    assertThat(response.getBody()).isEqualTo("{\"status\":\"Hello, World!\"}");
+    String expectedJson = "{\"status\":\"Hello, World!\"}";
+    JSONAssert.assertEquals(expectedJson, response.getBody(), JSONCompareMode.STRICT);
   }
 }
